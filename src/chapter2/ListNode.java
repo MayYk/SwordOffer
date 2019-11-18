6ae3f167b2799da87da08147c28420ea6652114c
MayYk <471519146@qq.com>
2019-11-18 21:53:50 +0800

    2019.11.18

--- src/chapter2/ListNode.java
+++ src/chapter2/ListNode.java
@@ -1,26 +1,37 @@
 package chapter2;
 
+import java.util.List;
+
 public class ListNode {
     public int val;
     public ListNode next = null;
-    
+
     public ListNode(){
-        
+        this.val = 0;
+        this.next = null;
     }
-    
+
     public ListNode(int val) {
         // TODO Auto-generated constructor stub
         this.val = val;
     }
 
+    public void addListNode(ListNode nextNode){
+        this.next = nextNode;
+    }
+
+//    这里toString不要改变原有结构
+//    如toString,改变了原有next指向
+//    debug查看变量内容时,会调用toString方法,影响了原有结构
     @Override
     public String toString(){
-        System.out.println(val);
-        while(next != null){
-            System.out.println(next.val);
-            next = next.next;
-        }
-        return "";
+//        System.out.println(val);
+//        System.out.println("das");
+//        while(next != null){
+//            System.out.println(next.val);
+//            next = next.next;
+//        }
+        return "" + val;
     }
-    
+
 }
